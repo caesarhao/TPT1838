698fe3d1f98e66a12769b86c2656e848c18c1667
caesarhao <caesarhao@gmail.com>
2023-01-04 19:07:24 +0100

    ec

--- EC_flash_ReverseEngineering/EC_flash_c/ec_flash.c
+++ EC_flash_ReverseEngineering/EC_flash_c/ec_flash.c
@@ -1,22 +1,22 @@
 
 
-disable_batterypolling
+disable_batterypolling ()
 
-read_ec_device
+read_ec_device (int32_t arg1, int32_t arg2)
 
-write_ec_device
+write_ec_device (int32_t arg1, int32_t arg2)
 
-KBC_CMD_DATAS
+KBC_CMD_DATAS (int32_t arg1, int32_t arg2)
 
-strlwr
+strlwr (char *arg1)
 
-HexStr2DWORD
+HexStr2DWORD (int32_t arg1)
 
-display_version
+display_version ()
 
-display_usage
+display_usage ()
 
-CRC8_Calculator
+CRC8_Calculator (int32_t arg1, int32_t arg2)
 
-main
+int main (int argc, char **argv)
 
